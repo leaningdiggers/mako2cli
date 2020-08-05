f62beef91c0411c6475f9f32a98fe893e63860ab
fbagagli <francesco.bagagli@gmail.com>
2020-08-05 16:35:12 +0200

    pinned environment on test and lint

--- noxfile.py
+++ noxfile.py
@@ -1,15 +1,30 @@
-import nox
 import tempfile
+
+import nox
 
 nox.options.sessions = "lint", "safety", "tests"
 
 locations = "src", "tests", "noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -31,25 +46,29 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock", "pyfakefs"
+    )
     session.run("pytest", *args)
 
 
 @nox.session(python="3.8")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python="3.8")
 def isort(session):
     args = session.posargs or locations
-    session.install("isort")
+    install_with_constraints(session, "isort")
     session.run("isort", *args)
