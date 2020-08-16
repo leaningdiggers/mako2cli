e3299f6b168db7d8edc7f58c364f2878f33f5fd6
fbagagli <francesco.bagagli@gmail.com>
2020-08-16 18:03:26 +0200

    fix: correctly commented for both exclusion (mypy and coverage)

--- src/mako2cli/__init__.py
+++ src/mako2cli/__init__.py
@@ -6,10 +6,10 @@ from .TemplateEngine import TemplateEngineException
 
 try:
     from importlib.metadata import PackageNotFoundError  # type: ignore
-    from importlib.metadata import version
+    from importlib.metadata import version  # type: ignore # pragma: no cover
 except ImportError:  # pragma: no cover
     from importlib_metadata import PackageNotFoundError  # type: ignore
-    from importlib_metadata import version
+    from importlib_metadata import version  # type: ignore
 
 
 try:
